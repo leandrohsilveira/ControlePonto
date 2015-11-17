00b15e455b2adeddd7bc78c946bf065836e81efd
leandrohsilveira <leandro.hinckel@gmail.com>
2015-11-17 18:07:39 -0200

    #9: Ajustes no servlet de Autenticação
    Ajustes no servlet de autenticação para adaptar a estrutura Base criada.

--- pontocontrol-web/src/main/java/br/com/lhs/pontocontrol/web/servlet/AutenticacaoServlet.java
+++ pontocontrol-web/src/main/java/br/com/lhs/pontocontrol/web/servlet/AutenticacaoServlet.java
@@ -3,32 +3,37 @@ package br.com.lhs.pontocontrol.web.servlet;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.lhs.pontocontrol.web.vo.UsuarioVO;
 import br.com.pontocontrol.controleponto.SessaoManager;
+import br.com.pontocontrol.controleponto.model.ConfiguracoesUsuario;
 
-public class AutenticacaoServlet extends HttpServlet {
+public class AutenticacaoServlet extends BaseServlet {
 
 	private static final long serialVersionUID = 3966806345787674814L;
 
 	@Override
 	protected void doPost(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
-		final String login = req.getParameter("login");
-		final int autenticar = SessaoManager.getInstance().autenticar(login);
-		switch (autenticar) {
-		case SessaoManager.LOGIN_STATUS.OK:
-		case SessaoManager.LOGIN_STATUS.JA_AUTENTICADO:
-			resp.setStatus(HttpServletResponse.SC_OK);
-			break;
-		case SessaoManager.LOGIN_STATUS.USUARIO_NAO_EXISTE:
-			resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-			break;
-		default:
-			resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-			break;
-		}
+		accept(MEDIA_TYPE.X_WWW_FORM_URLENCODED, req, resp, () -> {
+			final String login = req.getParameter("login");
+			final int autenticar = SessaoManager.getInstance().autenticar(login);
+			switch (autenticar) {
+			case SessaoManager.LOGIN_STATUS.OK:
+			case SessaoManager.LOGIN_STATUS.JA_AUTENTICADO:
+				final ConfiguracoesUsuario usuarioAutenticado = SessaoManager.getInstance().getUsuarioAutenticado();
+				final UsuarioVO usuarioVO = new UsuarioVO(usuarioAutenticado.getNome(), usuarioAutenticado.getLogin(), String.valueOf(usuarioAutenticado.getOffset()));
+				respondJson(resp, usuarioVO);
+				break;
+			case SessaoManager.LOGIN_STATUS.USUARIO_NAO_EXISTE:
+				respondError(resp, HttpServletResponse.SC_NOT_FOUND, "O usuário informado não existe!");
+				break;
+			default:
+				respondError(resp, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Ocorreu um erro inesperado ao processar a resposta.");
+				break;
+			}
+		});
 	}
 
 }
