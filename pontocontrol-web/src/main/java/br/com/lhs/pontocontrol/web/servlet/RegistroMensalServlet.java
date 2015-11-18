49e69ad6f2bccb50bc3e01127299fa483cad24bf
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2015-11-18 02:01:19 -0200

    #11: Melhorias no Servlet de RegistroMensal
    Melhoria no tratamento de exceções e respostas com JSON.

--- pontocontrol-web/src/main/java/br/com/lhs/pontocontrol/web/servlet/RegistroMensalServlet.java
+++ pontocontrol-web/src/main/java/br/com/lhs/pontocontrol/web/servlet/RegistroMensalServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.pontocontrol.controleponto.SessaoManager;
 import br.com.pontocontrol.controleponto.controller.ControllerFactory;
 import br.com.pontocontrol.controleponto.controller.IFolhaPontoController;
 import br.com.pontocontrol.controleponto.controller.json.impl.FolhaMensalPontoJSON;
@@ -17,16 +18,21 @@ public class RegistroMensalServlet extends BaseServlet {
 
 	@Override
 	protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
-		final Integer mes = Integer.valueOf(req.getParameter("mes"));
-		final Integer ano = Integer.valueOf(req.getParameter("ano"));
+		if (SessaoManager.getInstance().isUsuarioAutenticado()) {
+			final Integer mes = Integer.valueOf(req.getParameter("mes"));
+			final Integer ano = Integer.valueOf(req.getParameter("ano"));
 
-		final IFolhaPontoController folhaPontoController = ControllerFactory.localizar(IFolhaPontoController.class);
-		final FolhaMensalPontoJSON mensalPontoJSON = folhaPontoController.recuperarFolhaMensal(ano, mes);
-		if (mensalPontoJSON != null) {
-			respondJson(resp, mensalPontoJSON);
+			final IFolhaPontoController folhaPontoController = ControllerFactory.localizar(IFolhaPontoController.class);
+			final FolhaMensalPontoJSON mensalPontoJSON = folhaPontoController.recuperarFolhaMensal(ano, mes);
+			if (mensalPontoJSON != null) {
+				respondJson(resp, mensalPontoJSON);
+			} else {
+				respondJson(resp, new FolhaMensalPontoJSON(new FolhaMensalPonto()));
+			}
 		} else {
-			respondJson(resp, new FolhaMensalPontoJSON(new FolhaMensalPonto()));
+			respondError(resp, HttpServletResponse.SC_UNAUTHORIZED, "Você deve estar autenticado para acessar este conteúdo.");
 		}
+
 	}
 
 }
