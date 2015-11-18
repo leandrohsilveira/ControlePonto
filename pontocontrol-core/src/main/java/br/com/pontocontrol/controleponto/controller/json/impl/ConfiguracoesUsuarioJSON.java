4bf8f9c6506c28c0e8a4c5b62086ee3945429933
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2015-11-18 01:50:22 -0200

    #32: Ajuste para repassar o nome do usuário a classe JSON.

--- pontocontrol-core/src/main/java/br/com/pontocontrol/controleponto/controller/json/impl/ConfiguracoesUsuarioJSON.java
+++ pontocontrol-core/src/main/java/br/com/pontocontrol/controleponto/controller/json/impl/ConfiguracoesUsuarioJSON.java
@@ -16,53 +16,58 @@ import br.com.pontocontrol.controleponto.util.TimeUtils;
  */
 public class ConfiguracoesUsuarioJSON extends BaseJSON<ConfiguracoesUsuario, ConfiguracoesUsuarioJSON> {
 
-    public ConfiguracoesUsuarioJSON(ConfiguracoesUsuario model) {
-        login = model.getLogin();
-        segunda = model.isSegunda();
-        terca = model.isTerca();
-        quarta = model.isQuarta();
-        quinta = model.isQuinta();
-        sexta = model.isSexta();
-        sabado = model.isSabado();
-        domingo = model.isDomingo();
-        offset = model.getOffset();
-    }
-    
-    //login e user path.
-    public String login;
-    
-    //Expediente
-    public boolean segunda = true;
-    public boolean terca = true;
-    public boolean quarta = true;
-    public boolean quinta = true;
-    public boolean sexta = true;
-    public boolean sabado = false;
-    public boolean domingo = false;
-    
-    public long offset = TimeUtils.OFFSET_8_HORAS;
-    
-    public static ConfiguracoesUsuario toModel(ConfiguracoesUsuarioJSON json) {
-        ConfiguracoesUsuario configuracoesUsuario = new ConfiguracoesUsuario(json.login);
-        configuracoesUsuario.setSegunda(json.segunda);
-        configuracoesUsuario.setTerca(json.terca);
-        configuracoesUsuario.setQuarta(json.quarta);
-        configuracoesUsuario.setQuinta(json.quinta);
-        configuracoesUsuario.setSexta(json.sexta);
-        configuracoesUsuario.setSabado(json.sabado);
-        configuracoesUsuario.setDomingo(json.domingo);
-        configuracoesUsuario.setOffset(json.offset);
-        return configuracoesUsuario;
-    }
+	private static final long serialVersionUID = -2634865214265402219L;
 
-    @Override
-    public Class<ConfiguracoesUsuarioJSON> getJsonClass() {
-        return ConfiguracoesUsuarioJSON.class;
-    }
+	public ConfiguracoesUsuarioJSON(ConfiguracoesUsuario model) {
+		login = model.getLogin();
+		nome = model.getNome();
+		segunda = model.isSegunda();
+		terca = model.isTerca();
+		quarta = model.isQuarta();
+		quinta = model.isQuinta();
+		sexta = model.isSexta();
+		sabado = model.isSabado();
+		domingo = model.isDomingo();
+		offset = model.getOffset();
+	}
 
-    @Override
-    public ConfiguracoesUsuario toModel() {
-        return ConfiguracoesUsuarioJSON.toModel(this);
-    }
-    
+	// login e user path.
+	public String login;
+	public String nome;
+
+	// Expediente
+	public boolean segunda = true;
+	public boolean terca = true;
+	public boolean quarta = true;
+	public boolean quinta = true;
+	public boolean sexta = true;
+	public boolean sabado = false;
+	public boolean domingo = false;
+
+	public long offset = TimeUtils.OFFSET_8_HORAS;
+
+	public static ConfiguracoesUsuario toModel(ConfiguracoesUsuarioJSON json) {
+		final ConfiguracoesUsuario configuracoesUsuario = new ConfiguracoesUsuario(json.login);
+		configuracoesUsuario.setSegunda(json.segunda);
+		configuracoesUsuario.setTerca(json.terca);
+		configuracoesUsuario.setQuarta(json.quarta);
+		configuracoesUsuario.setQuinta(json.quinta);
+		configuracoesUsuario.setSexta(json.sexta);
+		configuracoesUsuario.setSabado(json.sabado);
+		configuracoesUsuario.setDomingo(json.domingo);
+		configuracoesUsuario.setOffset(json.offset);
+		configuracoesUsuario.setNome(json.nome);
+		return configuracoesUsuario;
+	}
+
+	@Override
+	public Class<ConfiguracoesUsuarioJSON> getJsonClass() {
+		return ConfiguracoesUsuarioJSON.class;
+	}
+
+	@Override
+	public ConfiguracoesUsuario toModel() {
+		return ConfiguracoesUsuarioJSON.toModel(this);
+	}
+
 }
