b1c6e1e649e66d1d52095aaf28fd3887d20ca816
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2015-11-18 01:47:35 -0200

    #31: Criado método "atualizarUsuario" no SessaoManager

--- pontocontrol-core/src/main/java/br/com/pontocontrol/controleponto/SessaoManager.java
+++ pontocontrol-core/src/main/java/br/com/pontocontrol/controleponto/SessaoManager.java
@@ -19,51 +19,57 @@ import br.com.pontocontrol.controleponto.model.ConfiguracoesUsuario;
  */
 public class SessaoManager extends BaseSessaoManager {
 
-   private SessaoManager() {
-   }
+	private SessaoManager() {
+	}
 
-   private static final Logger LOG = Logger.getLogger(SessaoManager.class.getName());
-   private static SessaoManager instance;
+	private static final Logger LOG = Logger.getLogger(SessaoManager.class.getName());
+	private static SessaoManager instance;
 
-   public interface LOGIN_STATUS {
+	public interface LOGIN_STATUS {
 
-      public static final int OK = 1;
-      public static final int USUARIO_NAO_EXISTE = 2;
-      public static final int JA_AUTENTICADO = 3;
-   }
+		public static final int OK = 1;
+		public static final int USUARIO_NAO_EXISTE = 2;
+		public static final int JA_AUTENTICADO = 3;
+	}
 
-   public static SessaoManager getInstance() {
-      if (instance == null) {
-         instance = new SessaoManager();
-      }
-      return instance;
-   }
+	public static SessaoManager getInstance() {
+		if (instance == null) {
+			instance = new SessaoManager();
+		}
+		return instance;
+	}
 
-   private final IConfiguracaoUsuarioController confUsuarioController = ControllerFactory.localizar(IConfiguracaoUsuarioController.class);
+	private final IConfiguracaoUsuarioController confUsuarioController = ControllerFactory.localizar(IConfiguracaoUsuarioController.class);
 
-   public int autenticar(String login) {
-      if (usuario == null) {
-         ConfiguracoesUsuarioJSON json = confUsuarioController.recuperarConfiguracaoUsuario(login);
-         if (json != null) {
-            usuario = json.toModel();
-            return LOGIN_STATUS.OK;
-         }
-         return LOGIN_STATUS.USUARIO_NAO_EXISTE;
-      } else {
-         LOG.warning(String.format("Já existe um usuário autenticado neste momento com o login \"%s\"", usuario.getLogin()));
-         return LOGIN_STATUS.JA_AUTENTICADO;
-      }
-   }
+	public int autenticar(String login) {
+		if (usuario == null) {
+			final ConfiguracoesUsuarioJSON json = confUsuarioController.recuperarConfiguracaoUsuario(login);
+			if (json != null) {
+				usuario = json.toModel();
+				return LOGIN_STATUS.OK;
+			}
+			return LOGIN_STATUS.USUARIO_NAO_EXISTE;
+		} else {
+			LOG.warning(String.format("Já existe um usuário autenticado neste momento com o login \"%s\"", usuario.getLogin()));
+			return LOGIN_STATUS.JA_AUTENTICADO;
+		}
+	}
 
-   public boolean criarUsuario(ConfiguracoesUsuario model) {
-      File usrDir = new File(PathsManager.getInstance().getPathUsuario(model.getLogin()));
-      if (usrDir.exists()) {
-         return false;
-      } else {
-         usrDir.mkdirs();
-         confUsuarioController.sincronizar(model);
-         return true;
-      }
-   }
+	public boolean criarUsuario(ConfiguracoesUsuario model) {
+		final File usrDir = new File(PathsManager.getInstance().getPathUsuario(model.getLogin()));
+		if (usrDir.exists()) {
+			return false;
+		} else {
+			usrDir.mkdirs();
+			confUsuarioController.sincronizar(model);
+			return true;
+		}
+	}
+
+	public void atualizarUsuario(ConfiguracoesUsuario model) {
+		if (!criarUsuario(model)) {
+			confUsuarioController.sincronizar(model);
+		}
+	}
 
 }
