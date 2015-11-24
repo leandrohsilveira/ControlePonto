4639fbc74f6a9e2f28745121af689c0fa46b1977
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2015-11-24 20:09:44 -0200

    #12: Implementação POST do Servlet de Registro Diario

--- pontocontrol-web/src/main/java/br/com/lhs/pontocontrol/web/servlet/RegistroDiarioServlet.java
+++ pontocontrol-web/src/main/java/br/com/lhs/pontocontrol/web/servlet/RegistroDiarioServlet.java
@@ -10,6 +10,7 @@ import br.com.pontocontrol.controleponto.controller.ControllerFactory;
 import br.com.pontocontrol.controleponto.controller.IFolhaPontoController;
 import br.com.pontocontrol.controleponto.controller.json.impl.FolhaMensalPontoJSON;
 import br.com.pontocontrol.controleponto.controller.json.impl.RegistroDiarioPontoJSON;
+import br.com.pontocontrol.controleponto.model.FolhaMensalPonto;
 import br.com.pontocontrol.controleponto.model.RegistroDiarioPonto;
 import br.com.pontocontrol.controleponto.util.DateUtils;
 
@@ -33,6 +34,24 @@ public class RegistroDiarioServlet extends BaseServlet {
 			}
 		}
 		respondJson(resp, new RegistroDiarioPontoJSON(new RegistroDiarioPonto(dia)));
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		acceptJson(req, resp, RegistroDiarioPontoJSON.class, (content) -> {
+			final Integer mes = getParameterAsIntegerOrDefault(req, "mes", DateUtils.getActualMonth());
+			final Integer ano = getParameterAsIntegerOrDefault(req, "ano", DateUtils.getActualYear());
+			final IFolhaPontoController controller = ControllerFactory.localizar(IFolhaPontoController.class);
+			final FolhaMensalPontoJSON folhaMensalJson = controller.recuperarFolhaMensal(ano, mes);
+			final FolhaMensalPonto folhaMensal = folhaMensalJson.toModel();
+			final RegistroDiarioPonto registroDiario = content.toModel();
+			final RegistroDiarioPonto previousValue = folhaMensal.getRegistros().putIfAbsent(registroDiario.getDia(), registroDiario);
+			if (previousValue != null) {
+				respondJson(resp, new RegistroDiarioPontoJSON(registroDiario));
+			} else {
+				respondError(resp, HttpServletResponse.SC_CONFLICT, "Já existe um registro diário para esta data.");
+			}
+		});
 	}
 
 }
